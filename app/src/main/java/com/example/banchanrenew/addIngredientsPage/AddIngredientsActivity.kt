32c5e422cfabac274cd00e54f643bdc0641ea468
Minchae0322 <wjdalsco12>
2022-06-02 18:31:14 +0900

    6-2_banchan

--- app/src/main/java/com/example/banchanrenew/addIngredientsPage/AddIngredientsActivity.kt
+++ app/src/main/java/com/example/banchanrenew/addIngredientsPage/AddIngredientsActivity.kt
@@ -1,17 +1,11 @@
 package com.example.banchanrenew.addIngredientsPage
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.banchanrenew.MainActivity
-import com.example.banchanrenew.MainActivity.Companion.db
-import com.example.banchanrenew.R
 import com.example.banchanrenew.databinding.ActivityAddfridgeBinding
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 /**
  * The number of pages (wizard steps) to show in this demo.
@@ -30,38 +24,26 @@ class AddIngredientsActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddfridgeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initPagerAdapter()
+        initTabLayout()
 
-        // Instantiate a ViewPager2 and a PagerAdapter.
-
-        // The pager adapter, which provides the pages to the view pager widget.
-        val pagerAdapter = ScreenSlidePagerAdapter(this)
-        binding.viewPagerAddIngredients.adapter = pagerAdapter
-        //https://korean-otter.tistory.com/entry/android-kotlin-TabLayout%EA%B3%BC-ViewPager%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
-        binding.tablayout.addTab(binding.tablayout.newTab().setCustomView(createCustomView("육류/가공품")))
-        binding.tablayout.addTab(binding.tablayout.newTab().setCustomView(createCustomView("채소")))
-        binding.tablayout.addTab(binding.tablayout.newTab().setCustomView(createCustomView("소스/향신료")))
-        binding.tablayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-
-        })
     }
 
-    private fun createCustomView(text: String): View? {
-        val inflater = LayoutInflater.from(applicationContext)
-        val view: View = inflater.inflate(R.layout.tab_custom, null)
-        val textView = view.findViewById<View>(R.id.tv_tab) as TextView
-        textView.text = text
-        return view
+    private fun initPagerAdapter() {
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        binding.viewPagerAddIngredients.adapter = pagerAdapter
+    }
+
+    private fun initTabLayout() {
+        val tabTitleArray = arrayOf(
+            "육류/가공품",
+            "야채",
+            "소스/향신료"
+        )
+
+        TabLayoutMediator(binding.tablayout, binding.viewPagerAddIngredients) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 
     /**
@@ -74,13 +56,13 @@ class AddIngredientsActivity : FragmentActivity() {
         override fun createFragment(position: Int): Fragment {
             return when(position) {
                 0 -> {
-                    MeatPagerFragment(MainActivity.db.testDao().selectIngredientWhereDataType("meat"))
+                    MeatPagerFragment()
                 }
                 1 -> {
-                    VegetablePagerFragment(db.testDao().find())
+                    VegetablePagerFragment()
                 }
                 else -> {
-                    FishPagerFragment(db.testDao().find())
+                    SourcePagerFragment()
                 }
             }
 
---
+++ app/src/main/java/com/example/banchanrenew/addIngredientsPage/AddIngredientsDialog.kt
@@ -0,0 +1,237 @@
+package com.example.banchanrenew.addIngredientsPage
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.text.Editable
+import android.text.InputFilter
+import android.text.Spanned
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.WindowManager
+import com.example.banchanrenew.MainActivity.Companion.db
+import com.example.banchanrenew.R
+import com.example.banchanrenew.databinding.DialogAddBinding
+import com.example.banchanrenew.relation.Ingredient
+
+
+class AddIngredientsDialog(val context: Context, var ingredient: Ingredient, val adapter:AddIngredientsAdapter) {
+    private val dialog = Dialog(context)
+    private lateinit var binding: DialogAddBinding
+    private var buttonNum: Int = 0
+    private var editTextGram: Int = 100
+    private var remain: Int = ingredient.remainGram + 100
+
+    fun showDialog() {
+        binding = DialogAddBinding.bind(LayoutInflater.from(context).inflate(R.layout.dialog_add, null))
+        initDialog()
+        bindView()
+
+    }
+
+    private fun initDialog() {
+        dialog.setContentView(binding.root)
+        dialog.show()
+        dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun bindView() {
+        binding.tvAddDialogName.text = ingredient.name
+        binding.tvAddDialogRemain.text = "보유수량 :  " + ingredient.remainGram.toString() + "g"
+        initMenuView()
+        initEditTextView()
+        initCheckView()
+        updateAfterChangeTextView()
+    }
+
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initCheckView() {
+        binding.tvAddDialogCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        binding.tvAddDialogOk.setOnClickListener {
+            updateRemain()
+            db.recipeDao().updateRemainOfIngredient(remain,ingredient.id)
+            adapter.updateList()
+            dialog.dismiss()
+        }
+    }
+
+    private fun initEditTextView() {
+        binding.etAddDialogAmount.hint = "0"
+        updateEditTextView()
+        binding.etAddDialogAmount.filters = arrayOf<InputFilter>(MinMaxFilter(1, 100000))
+        binding.etAddDialogAmount.setSelection( binding.etAddDialogAmount.length())
+        binding.etAddDialogAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                editTextGram = if(p0.toString() == "") {
+                    0
+                } else {
+                    p0.toString().toInt()
+                }
+
+            }
+            override fun afterTextChanged(p0: Editable?) {}
+
+        })
+        binding.tvMinus.setOnClickListener {
+            if(editTextGram <= 100) {
+                editTextGram = 0
+            } else {
+                editTextGram -= 100
+            }
+            updateViews()
+        }
+        binding.tvPlus.setOnClickListener {
+            when(buttonNum) {
+                0 -> {
+                    editTextGram += 100
+                    updateViews()
+                }
+                1 -> {
+                    if(ingredient.remainGram - 100 < editTextGram) {
+                        editTextGram = ingredient.remainGram
+                    } else {
+                        editTextGram += 100
+                        updateViews()
+                    }
+                }
+                2 -> {
+                    editTextGram = ingredient.remainGram
+                    updateViews()
+                }
+            }
+
+        }
+    }
+
+    private fun initMenuView() {
+        binding.tvAddDialogPlus.setOnClickListener {
+            binding.etAddDialogAmount.filters = arrayOf<InputFilter>(MinMaxFilter(1, 100000))
+            menuUnClicked(0)
+            menuClicked(0)
+            updateViews()
+        }
+        binding.tvAddDialogRemove.setOnClickListener {
+            binding.etAddDialogAmount.filters = arrayOf<InputFilter>(MinMaxFilter(0, ingredient.remainGram))
+            menuUnClicked(1)
+            menuClicked(1)
+            updateViews()
+        }
+        binding.tvAddDialogRemoveAll.setOnClickListener {
+            binding.etAddDialogAmount.filters = arrayOf<InputFilter>(MinMaxFilter(0, ingredient.remainGram))
+            menuUnClicked(2)
+            menuClicked(2)
+            updateViews()
+        }
+    }
+
+    private fun menuClicked(count: Int) {
+        when(count) {
+            0 -> {
+                binding.tvAddDialogPlus.setBackgroundResource(R.drawable.background_colorrounding)
+                binding.tvAddDialogPlus.setTextColor(Color.WHITE)
+                editTextGram = 100
+            }
+            1 -> {
+                binding.tvAddDialogRemove.setBackgroundResource(R.drawable.background_colorrounding)
+                binding.tvAddDialogRemove.setTextColor(Color.WHITE)
+                editTextGram = 0
+            }
+            2 -> {
+                binding.tvAddDialogRemoveAll.setBackgroundResource(R.drawable.background_colorrounding)
+                binding.tvAddDialogRemoveAll.setTextColor(Color.WHITE)
+                editTextGram = ingredient.remainGram
+            }
+        }
+        buttonNum =  count
+    }
+
+    private fun menuUnClicked(count: Int) {
+        if(buttonNum != count) {
+            when (buttonNum) {
+                0 -> {
+                    binding.tvAddDialogPlus.setBackgroundResource(R.drawable.background_rounding)
+                    binding.tvAddDialogPlus.setTextColor(Color.parseColor("#919191"))
+                }
+                1 -> {
+                    binding.tvAddDialogRemove.setBackgroundResource(R.drawable.background_rounding)
+                    binding.tvAddDialogRemove.setTextColor(Color.parseColor("#919191"))
+                }
+                2 -> {
+                    binding.tvAddDialogRemoveAll.setBackgroundResource(R.drawable.background_rounding)
+                    binding.tvAddDialogRemoveAll.setTextColor(Color.parseColor("#919191"))
+                }
+            }
+        }
+    }
+
+    private fun updateViews() {
+        updateEditTextView()
+        updateRemain()
+        updateAfterChangeTextView()
+    }
+
+    private fun updateEditTextView() {
+        if(editTextGram == 0) {
+            binding.etAddDialogAmount.setText("")
+        } else {
+            binding.etAddDialogAmount.setText(editTextGram.toString())
+        }
+    }
+
+    private fun updateAfterChangeTextView() {
+        binding.tvAddDialogAfterChange.text = remain.toString() + "g"
+    }
+
+    private fun updateRemain() {
+        when(buttonNum) {
+            0 -> {
+                remain = ingredient.remainGram + editTextGram
+            }
+            1 -> {
+                remain = ingredient.remainGram - editTextGram
+            }
+            2 -> {
+                remain = ingredient.remainGram - editTextGram
+            }
+        }
+    }
+
+
+    inner class MinMaxFilter() : InputFilter {
+        private var intMin: Int = 0
+        private var intMax: Int = 0
+
+        // Initialized
+        constructor(minValue: Int, maxValue: Int) : this() {
+            this.intMin = minValue
+            this.intMax = maxValue
+        }
+
+        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dStart: Int, dEnd: Int): CharSequence? {
+            try {
+                val input = Integer.parseInt(dest.toString() + source.toString())
+                if (isInRange(intMin, intMax, input)) {
+                    return null
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+            return ""
+        }
+
+        // Check if input c is in between min a and max b and
+        // returns corresponding boolean
+        private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+            return if (b > a) c in a..b else c in b..a
+        }
+    }
+}
